f0c1c99485425ecbd0d01da34b6f6f94e89e97e1
Mor Khojasteh <mori.kh1987@gmail.com>
2023-01-23 23:49:59 +0000

    feat: Mongoose setup + Envalid

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,11 @@
-import express from "express";
-const app = express();
-const port = process.env.PORT || 5000;
+import app from './app';
+import env from "./util/validateEnv";
+import mongoose from "mongoose";
 
-app.get("/", (req, res) => {
-  res.send("Welcome to server!");
-});
-
-
-app.listen(port, () => console.log(`Listening on port ${port}`));
+const port = env.PORT || 5000;
+mongoose.connect(env.MONGO_CONNECTIONS)
+.then(() => {
+  console.log("MongoDB is connected!");
+  app.listen(port, () => console.log(`Listening on port ${port}`));
+})
+.catch(err => console);
